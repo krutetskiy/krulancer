aa1488b32599356242143c2afaadb44e58e324b4
Krutetskiy Semyn <skrutetskiy@ozon.ru>
2022-12-30 17:42:42 +0300

    produce on drag task

--- src/components/ProjectTask.tsx
+++ src/components/ProjectTask.tsx
@@ -1,5 +1,5 @@
 import { Component, useRef, useState } from "react";
-import Draggable, { DraggableData } from "react-draggable";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 
 export enum PriorityType {
     Low = "Low",
@@ -24,64 +24,86 @@ export interface ProjectTaskModel {
 
 export interface IProps extends ProjectTaskModel { }
 
-function getPriorityColor(priority: PriorityType) {
-    switch (priority) {
-        case PriorityType.Low: {
-            return '#67CB65'
-        }
-        case PriorityType.Medium: {
-            return '#FF9533'
-        }
-        case PriorityType.High: {
-            return '#E74444'
-        }
-    }
+export interface IProjectTask extends IProps {
+    onDragTask: (e: DraggableEvent, data: DraggableData) => void
 }
 
-export interface IProjectTask {
-    onDragTask: () => void
-}
+class ProjectTask extends Component<IProjectTask, any>  {
+    constructor(props: IProjectTask) {
+        super(props);
 
-const ProjectTask = ({ title, assigned, estimated, priority } : IProps) => {
-    const nodeRef = useRef(null)
-    const [x, setX] = useState(0)
-    const [y, setY] = useState(0)
+        this.state = {
+            task: {
+                title: props.title,
+                assigned: props.assigned,
+                estimated: props.estimated,
+                priority: props.priority,
+                status: props.status
+            }
+        }
 
-    const handleStop = (event: any, info: DraggableData) => {
-        setX(info.deltaX)
-        setY(info.deltaY)
+        this.onDragTask = this.onDragTask.bind(this)
     }
 
-    const handleStart = (event: any, info: DraggableData) => {
-        const parentNode = info.node.parentNode;
-        console.log(parentNode)
+    onDragTask(e: DraggableEvent, data: DraggableData): void {
+        const task = this.state.task;
+        if (task) {
+            this.props.onDragTask(e, data);
+        }
     }
 
-    return (
-        <>
-        <Draggable
+    onDragStart(e: DraggableEvent, data: DraggableData): void {
+        data.node.style.zIndex = '50';
+    }
+
+    onDragStop(e: DraggableEvent, data: DraggableData): void {
+        data.node.style.zIndex = '0';
+    }
+
+    getPriorityColor(priority: PriorityType) {
+        switch (priority) {
+            case PriorityType.Low: {
+                return '#67CB65'
+            }
+            case PriorityType.Medium: {
+                return '#FF9533'
+            }
+            case PriorityType.High: {
+                return '#E74444'
+            }
+        }
+    }
+
+    render() {
+        const { task, nodeRef } = this.state;
+
+        return (
+            <>
+            <Draggable
             nodeRef={nodeRef}
-            onStart={handleStart}
-            onStop={handleStop}
-            position={{x: x, y: y}}
+            onStart={this.onDragStart}
+            onDrag={this.onDragTask}
+            onStop={this.onDragStop}
+            position={{x: 0, y: 0}}
             >
             <div ref={nodeRef} className="flex flex-col my-1 px-4 py-2 bg-gray-task rounded-2xl cursor-grab">
               <div className="flex items-baseline mb-3">
-                    <h3 className="mr-2 font-mono font-medium">{title}</h3>
-                    <div className={`px-2 bg-[${getPriorityColor(priority)}] text-white rounded-2xl`}>{priority}</div>
+                    <h3 className="mr-2 font-mono font-medium">{task.title}</h3>
+                    <div className={`px-2 bg-[${this.getPriorityColor(task.priority)}] text-white rounded-2xl`}>{task.priority}</div>
                 </div>
                     <div className="flex justify-between">
                         <div className="flex font-mono font-mono font-medium text-gray-light">Assigned:</div>
-                        <div className="flex font-mono">{assigned}</div>
+                        <div className="flex font-mono">{task.assigned}</div>
                     </div>
                 <div className="flex justify-between">
                     <div className="flex font-mono font-mono font-medium text-gray-light">Estimated SP:</div>
-                    <div className="flex font-mono">{estimated}</div>
+                    <div className="flex font-mono">{task.estimated}</div>
                 </div>
             </div>
         </Draggable>
-        </>
-    )
+            </>
+        )
+    }
 }
 
 export default ProjectTask;
