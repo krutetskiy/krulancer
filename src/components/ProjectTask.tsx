e7fbb1237339c9fd70cc858b2e04a84211da2e6d
Krutetskiy Semyn <skrutetskiy@ozon.ru>
2022-12-31 15:59:27 +0300

    fix ticket change status

--- src/components/ProjectTask.tsx
+++ src/components/ProjectTask.tsx
@@ -24,57 +24,41 @@ export interface ProjectTaskModel {
 
 export interface IProps extends ProjectTaskModel { }
 
-class ProjectTask extends Component<IProps, any>  {
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-            task: {
-                title: props.title,
-                assigned: props.assigned,
-                estimated: props.estimated,
-                priority: props.priority,
-                status: props.status
-            }
+function getPriorityColor(priority: PriorityType) {
+    switch (priority) {
+        case PriorityType.Low: {
+            return '#67CB65'
+        }
+        case PriorityType.Medium: {
+            return '#FF9533'
+        }
+        case PriorityType.High: {
+            return '#E74444'
         }
     }
+}
 
-    getPriorityColor(priority: PriorityType) {
-        switch (priority) {
-            case PriorityType.Low: {
-                return '#67CB65'
-            }
-            case PriorityType.Medium: {
-                return '#FF9533'
-            }
-            case PriorityType.High: {
-                return '#E74444'
-            }
-        }
-    }
-
-    render() {
-        const { task } = this.state;
-
-        return (
-        <>
+const ProjectTask = (props: IProps) => {
+    const  { title, assigned, estimated, priority } = props
+    
+    return (
+    <>
         <div className="flex flex-col my-1 px-4 py-2 bg-gray-task rounded-2xl cursor-grab">
           <div className="flex items-baseline mb-3">
-                <h3 className="mr-2 font-mono font-medium">{task.title}</h3>
-                <div className={`px-2 bg-[${this.getPriorityColor(task.priority)}] text-white rounded-2xl`}>{task.priority}</div>
+                <h3 className="mr-2 font-mono font-medium">{title}</h3>
+                <div className={`px-2 bg-[${getPriorityColor(priority)}] text-white rounded-2xl`}>{priority}</div>
             </div>
                 <div className="flex justify-between">
                     <div className="flex font-mono font-mono font-medium text-gray-light">Assigned:</div>
-                    <div className="flex font-mono">{task.assigned}</div>
+                    <div className="flex font-mono">{assigned}</div>
                 </div>
             <div className="flex justify-between">
                 <div className="flex font-mono font-mono font-medium text-gray-light">Estimated SP:</div>
-                <div className="flex font-mono">{task.estimated}</div>
+                <div className="flex font-mono">{estimated}</div>
             </div>
         </div>
-        </>
-        )
-    }
+    </>
+    )
 }
 
 export default ProjectTask;
