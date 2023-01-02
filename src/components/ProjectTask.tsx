34a9c6941543188a93dd75045670b1bce65e6a92
Krutetskiy Semyn <skrutetskiy@ozon.ru>
2023-01-02 16:02:30 +0300

    get priority colors by map

--- src/components/ProjectTask.tsx
+++ src/components/ProjectTask.tsx
@@ -32,26 +32,18 @@ export interface IProps {
 const ProjectTask = (props: IProps) => {
     const { title, assigned, estimated, priority } = props
 
-    const getPriorityColor = (priority: PriorityType) => {
-        switch (priority) {
-            case PriorityType.Low: {
-                return '#67CB65'
-            }
-            case PriorityType.Medium: {
-                return '#FF9533'
-            }
-            case PriorityType.High: {
-                return '#E74444'
-            }
-        }
-    }
+    const priorityColor = new Map<PriorityType, string>([
+        [PriorityType.Low, "#67CB65"],
+        [PriorityType.Medium, "#FF9533"],
+        [PriorityType.High, "#E74444"],
+    ])
 
     return (
         <>
             <div className="flex flex-col my-1 px-4 py-2 bg-gray-task rounded-2xl cursor-grab">
                 <div className="flex items-baseline mb-3">
                     <h3 className="mr-2 font-mono font-medium">{title}</h3>
-                    <div className={`px-2 bg-[${getPriorityColor(priority)}] text-white rounded-2xl`}>{priority}</div>
+                    <div className={`px-2 bg-[${priorityColor.get(priority)}] text-white rounded-2xl`}>{priority}</div>
                 </div>
                 <div className="flex justify-between">
                     <div className="flex font-mono font-mono font-medium text-gray-light">Assigned:</div>
