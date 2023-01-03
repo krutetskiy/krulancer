06f86a9873db5147cf04e18f3e8a6c2f51f85329
Krutetskiy Semyn <skrutetskiy@ozon.ru>
2023-01-04 02:42:13 +0300

    fix inline priority color

--- src/components/ProjectDashboard/Task.tsx
+++ src/components/ProjectDashboard/Task.tsx
@@ -7,21 +7,19 @@ export interface IProps {
     priority: TaskPriorityType
 }
 
-const ProjectTask = (props: IProps) => {
-    const { title, assigned, estimated, priority } = props
+const priorityColorMap = new Map<string, string>([
+    [TaskPriorityType.Low, "bg-[#67CB65]"],
+    [TaskPriorityType.Medium, "bg-[#FF9533]"],
+    [TaskPriorityType.High, "bg-[#E74444]"],
+])
 
-    const priorityColor = new Map<TaskPriorityType, string>([
-        [TaskPriorityType.Low, "#67CB65"],
-        [TaskPriorityType.Medium, "#FF9533"],
-        [TaskPriorityType.High, "#E74444"],
-    ])
-
+const ProjectTask = ({ title, assigned, estimated, priority }: IProps) => {
     return (
         <>
             <div className="flex flex-col my-1 px-4 py-2 bg-gray-task rounded-2xl cursor-grab">
                 <div className="flex items-baseline mb-3">
                     <h3 className="mr-2 font-mono font-medium">{title}</h3>
-                    <div className={`px-2 bg-[${priorityColor.get(priority)}] text-white rounded-2xl`}>{priority}</div>
+                    <div className={`px-2 ${priorityColorMap.get(priority)} text-white rounded-2xl`}>{priority}</div>
                 </div>
                 <div className="flex justify-between">
                     <div className="flex font-mono font-mono font-medium text-gray-light">Assigned:</div>
