2c004733f2799a39f1f003b8d78dd6feee0bcb1e
Krutetskiy Semyn <skrutetskiy@ozon.ru>
2023-01-06 04:38:34 +0300

    auth by credentials

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useState, FormEvent } from "react";
+import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const [credits, setCredits] = useState<{ login: string, password: string }>({ login: "kingsemyn@gmail.com", password: "pwdpwdpwd" });
+  const mutateCredits = trpc.auth.signUp.useMutation()
+
+  const handleLoginInput = (event: FormEvent<HTMLInputElement>) => setCredits({
+    login: event.currentTarget.value,
+    password: credits.password
+  })
+
+  const handlePasswordInput = (event: FormEvent<HTMLInputElement>) => setCredits({
+    login: credits.login,
+    password: event.currentTarget.value
+  })
+
+  const handleCredentialSignUp = (e: FormEvent<HTMLButtonElement>) => {
+    mutateCredits.mutate({ login: credits.login, password: credits.password })
+    handleCredentialSignIn(e)
+  }
+
+  const handleCredentialSignIn = (e: FormEvent<HTMLButtonElement>) => {
+    signIn("credentials", { callbackUrl: "/dashboard", redirect: true, email: credits.login, password: credits.password })
+  }
+
   return (
     <>
       <Head>
@@ -14,10 +36,13 @@ const Home: NextPage = () => {
       <div className="flex flex-col justify-center items-center w-screen h-screen bg-gray-regular-2">
         <div className="flex flex-col p-20 rounded-2xl w-[600px] h-[700px] bg-gray-regular-1">
           <h1 className="font-mono self-center font-medium text-4xl tracking-widest">KRULANCER</h1>
-          <form className="flex flex-col my-12">
-            <input placeholder="Username" className="text-xl rounded-2xl px-4 py-2 w-full border-2 hover:border-blue-500 bg-gray-regular-1" type="text" />
-            <input placeholder="Password" className="text-xl rounded-2xl px-4 py-2 my-3 w-full border-2 hover:border-blue-500 bg-gray-regular-1" type="text" />
-            <button className="p-2 w-full text-2xl font-medium tracking-widest rounded-2xl bg-blue-500 text-white hover:border-blue-500 border-2 hover:bg-gray-regular-1 hover:text-black" type="submit">LogIn</button>
+          <form className="flex flex-col mt-12 mb-4">
+            <input value={"kingsemyn@gmail.com"} onChange={handleLoginInput} placeholder="Username" className="text-xl rounded-2xl px-4 py-2 w-full border-2 hover:border-blue-500 bg-gray-regular-1" type="text" />
+            <input value={"pwdpwdpwd"} onChange={handlePasswordInput} placeholder="Password" className="text-xl rounded-2xl px-4 py-2 my-3 w-full border-2 hover:border-blue-500 bg-gray-regular-1" type="password" />
+            <div className="flex justify-between">
+              <button onClick={handleCredentialSignUp} className="p-2 w-full text-2xl font-medium tracking-widest rounded-2xl bg-blue-500 text-white hover:border-blue-500 border-2 hover:bg-gray-regular-1 hover:text-black" type="submit">Sign Up</button>
+              <button onClick={handleCredentialSignIn} className="p-2 w-full text-2xl font-medium tracking-widest rounded-2xl bg-blue-500 text-white hover:border-blue-500 border-2 hover:bg-gray-regular-1 hover:text-black" type="submit">Sign In</button>
+            </div>
           </form>
           <hr className="border-1" />
           <button onClick={() => signIn("yandex", {
--- src/server/trpc/router/task.ts
+++ src/server/trpc/router/task.ts
@@ -4,45 +4,45 @@ import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const taskRouter = router({
-    updateTaskStatus: publicProcedure
-        .input(z.object(
-            {
-                taskId: z.number().nullish(),
-                status: z.enum([TaskStatusType.ToDo, TaskStatusType.InProgress, TaskStatusType.Closed, TaskStatusType.Frozen]).nullish()
-            }))
-        .mutation(({ ctx, input }) => {
-            if (!input.taskId || !input.status)
-                return null
+  updateTaskStatus: publicProcedure
+    .input(z.object(
+      {
+        taskId: z.number().nullish(),
+        status: z.enum([TaskStatusType.ToDo, TaskStatusType.InProgress, TaskStatusType.Closed, TaskStatusType.Frozen]).nullish()
+      }))
+    .mutation(({ ctx, input }) => {
+      if (!input.taskId || !input.status)
+        return null
 
-            return ctx.prisma.task.update({
-                where: {
-                    id: input.taskId
-                },
-                data: {
-                    status: input.status
-                }
-            })
-        }),
-    createTask: publicProcedure
-        .input(z.object(
-            {
-                title: z.string().min(1).max(100),
-                assigned: z.string().min(1).max(20),
-                estimated: z.number().min(0).max(100),
-                priority: z.enum([TaskPriorityType.Low, TaskPriorityType.Medium, TaskPriorityType.High]),
-                projectId: z.number().min(0)
-            }
-        ))
-        .mutation(({ ctx, input }) => {
-            return ctx.prisma.task.create({
-                data: {
-                    title: input.title,
-                    assigned: input.assigned,
-                    estimated: input.estimated,
-                    priority: input.priority,
-                    status: TaskStatusType.ToDo,
-                    projectId: input.projectId
-                }
-            })
-        })
+      return ctx.prisma.task.update({
+        where: {
+          id: input.taskId
+        },
+        data: {
+          status: input.status
+        }
+      })
+    }),
+  createTask: publicProcedure
+    .input(z.object(
+      {
+        title: z.string().min(1).max(100),
+        assigned: z.string().min(1).max(20),
+        estimated: z.number().min(0).max(100),
+        priority: z.enum([TaskPriorityType.Low, TaskPriorityType.Medium, TaskPriorityType.High]),
+        projectId: z.number().min(0)
+      }
+    ))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.task.create({
+        data: {
+          title: input.title,
+          assigned: input.assigned,
+          estimated: input.estimated,
+          priority: input.priority,
+          status: TaskStatusType.ToDo,
+          projectId: input.projectId
+        }
+      })
+    })
 })
