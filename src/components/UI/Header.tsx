ff793901ee53c45ad88a4bdb2310aaaf67910c50
Krutetskiy Semyn <skrutetskiy@ozon.ru>
2023-01-11 00:08:02 +0300

    ad logo into header

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <>
       <div className="flex flex-row justify-end p-3 shadow-md">
+        <h1 className="mr-20 font-mono self-center font-medium text-4xl tracking-widest">KRULANCER</h1>
         <form className="relative w-2/3 ml-5 mx-[100px]">
           <svg width="20" height="20" fill="currentColor" className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
             <path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
